8f0f70849175387e4a03c11421c03651eb734e0c
Krasimira Badova <krasi_b2@abv.bg>
2016-01-10 14:31:00 +0200

    change

--- week01/diveintopython.py
+++ week01/diveintopython.py
@@ -1,7 +1,119 @@
-from firstDay import to_digits
-from firstDay import to_character
-from firstDay import palindrome
 import copy
+
+
+def sum_of_digits(n):
+    n = abs(n)
+    sum = 0
+    for i in range(0, len(str(n))):
+        sum += int(str(n)[i])
+    return sum
+
+# print(sum_of_digits(1325132435356))
+
+
+def to_digits(n):
+    list_of_digits = []
+    for i in str(n):
+        list_of_digits.append(int(i))
+    return list_of_digits
+
+
+def to_number(digits):
+    string = ""
+    for item in digits:
+        string += str(item)
+    return int(string)
+
+
+def factorial(n):
+    fact = 1
+    while n != 1:
+        fact *= n
+        n = n - 1
+    return fact
+
+
+def fact_digits(n):
+    list_of_digits = to_digits(n)
+    sum = 0
+    for item in list_of_digits:
+        sum += factorial(item)
+    return sum
+
+
+def char_histogram(string):
+    list1 = []
+    list2 = []
+    for symbol in string:
+        list1.append(symbol)
+        list2.append(list1.count(symbol))
+    dictionary = dict(zip(list1, list2))
+    return dictionary
+
+
+def count_vowels(str):
+    count = 0
+    vowels = "aoeyiu"
+    for element in str.lower():
+        for vowel in vowels:
+            if element == vowel:
+                count += 1
+    return count
+
+
+def to_character(string):
+    list_of_digits = []
+    for item in string:
+        list_of_digits.append(item)
+    return list_of_digits
+
+
+def count_consonants(str):
+    count = 0
+    consonants = "bcdfghjklmnpqrstvwxz"
+    for element in str.lower():
+        if element in consonants:
+            count += 1
+    return count
+
+
+def fibonacci(n):
+    list_of_first_n_numbers = []
+    count_steps = 1
+    a, b = 1, 1
+    c = a + b
+    list_of_first_n_numbers.append(a)
+    while count_steps != n:
+        a = b
+        b = c
+        c = a + b
+        list_of_first_n_numbers.append(a)
+        count_steps = count_steps + 1
+    return list_of_first_n_numbers
+
+# print(fibonacci(10))
+
+
+def fib_number(n):
+    return to_number(fibonacci(n))
+
+# print(fib_number(8))
+
+
+def palindrome(obj):
+    if type(obj) == int:
+        list_of_digits = to_digits(obj)
+    else:
+        list_of_digits = to_character(obj)
+    i = 0
+    j = len(list_of_digits) - 1
+    while i < int(len(list_of_digits) / 2):
+        if list_of_digits[i] == list_of_digits[j]:
+            i += 1
+            j -= 1
+        else:
+            return False
+    return True
 
 
 def is_number_balanced(n):
@@ -73,7 +185,7 @@ def is_anagram(a, b):
     if len(a) != len(b):
         return False
     for i in to_character(a):
-        if not i in to_character(b):
+        if i not in to_character(b):
             return False
     return True
 
@@ -175,4 +287,5 @@ def matrix_bombing(matrix):
         bomb_counter += 1
     return result
 
-print(matrix_bombing([[1, 2, 3], [4, 5, 6], [7, 8, 9]]))
+# print(matrix_bombing([[1, 2, 3], [4, 5, 6], [7, 8, 9]]))
+
--- week01/final_round.py
+++ week01/final_round.py
@@ -1,5 +1,12 @@
-from firstDay import to_character
 import re
+
+
+def to_character(string):
+    list_of_digits = []
+    for item in string:
+        list_of_digits.append(item)
+    return list_of_digits
+
 
 def count_words(arr):
     dictionary = dict()
@@ -86,4 +93,47 @@ def gas_stations(distance, tank_size, stations):
             result.append(maxi)
     return result
 
-print(gas_stations(320, 90, [50, 80, 140, 180, 220, 290]))
+# print(gas_stations(320, 90, [50, 80, 140, 180, 220, 290]))
+
+def dictionary(number, count):
+    dictionary = {}
+    dictionary[0] = [" "]
+    dictionary[2] = ["a", "b", "c"]
+    dictionary[3] = ["d", "e", "f"]
+    dictionary[4] = ["g", "h", "i"]
+    dictionary[5] = ["j", "k", "l"]
+    dictionary[6] = ["m", "n", "o"]
+    dictionary[7] = ["p", "q", "r", "s"]
+    dictionary[8] = ["t", "u", "v"]
+    dictionary[9] = ["w", "x", "y", "z"]
+    if count >= len(dictionary[number]):
+        return dictionary[number][count - number - 1]
+    return dictionary[number][count]
+
+
+def numbers_to_message(sequence):
+    string = ""
+    index = 0
+    for i in range(0, len(sequence)):
+        if index+1 < len(sequence) and i < index:
+            i = index + 1
+        if sequence[i] == -1:
+            i += 1
+        if sequence[i] == 1:
+            count = 0
+            while i+1 < len(sequence) and sequence[i+1] == sequence[i+2]:
+                count += 1
+                i += 1
+            string += (dictionary(sequence[i], count)).upper()
+            i += 2
+        count = 0
+        while i+1 < len(sequence) and sequence[i] == sequence[i+1]:
+            count += 1
+            i += 1
+        string += dictionary(sequence[i], count)
+        index = i
+        if index == len(sequence)-1:
+            break
+    return string
+
+# print(numbers_to_message([1, 4, 4, 4, 8, 8, 8, 6, 6, 6, 0, 3, 3, 0, 1, 7, 7, 7, 7, 7, 2, 6, 6, 3, 2]))
